9400507434495421c421575ba61afe8eff526b4d
JasonChien <s124202468@gmail.com>
2018-07-03 16:52:10 +0800

    Git init.

---
+++ app/src/main/java/com/chiachen/mvpkt/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chiachen.mvpkt
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
