5c904d423520c05fb108af982d7948ca93025544
JasonChien <s124202468@gmail.com>
2018-07-03 17:57:32 +0800

    Add ref address.

--- app/src/main/java/com/chiachen/mvpkt/model/MainActivityModel.kt
+++ app/src/main/java/com/chiachen/mvpkt/model/MainActivityModel.kt
@@ -6,6 +6,7 @@ import com.chiachen.mvpkt.contract.ContractInterface
  * Created by jianjiacheng on 2018/7/3.
  */
 
+//https://medium.com/mindorks/android-mvp-pattern-debunked-for-beginners-kotlin-c56c888222e0
 class MainActivityModel : ContractInterface.Model {
 
     private var mCounter = 0
